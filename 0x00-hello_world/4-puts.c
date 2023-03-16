8ea73ae7df84346f2060e98a1d6b7bb905c1d085
Abdessamad <elfadili.ae@gmail.com>
2023-03-16 04:58:30 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ *puts function
+ *
+ *print a message using puts
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

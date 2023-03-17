148421aba7d8f04082153909db32e5c673480d2e
Abdessamad <elfadili.ae@gmail.com>
2023-03-17 14:27:01 -0700

    init value fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,13 +17,13 @@ int main(void)
 
 	for (i = 0; i < 99; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = 1; j < 100; j++)
 		{
-			putchar((i / 10)%10 + '0');
-			putchar(i%10 + '0');
+			putchar((i / 10) % 10 + '0');
+			putchar(i % 10 + '0');
 			putchar(' ');
-			putchar((j / 10)%10 + '0');
-			putchar(j%10 + '0');
+			putchar((j / 10) % 10 + '0');
+			putchar(j % 10 + '0');
 			if (i != 98 || j != 99)
 			putchar(',');
 			putchar(' ');

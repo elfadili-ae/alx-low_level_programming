a2a07a517beb26a243881584920edb2dd45fbb9f
Abdessamad <elfadili.ae@gmail.com>
2023-03-21 04:58:42 -0700

    int_islower function added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * int_islower - cehck lowercase
+ *
+ * Description: return 1 if it's lowercase and 0 if not
+ * @c : ASCII code of the character
+ *
+ * Return: 1 (lowercase) 0 (otherwise
+*/
+
+void int_islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}

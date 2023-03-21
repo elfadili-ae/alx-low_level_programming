74921f2cfed3287cc0983edb64e7132536885478
Abdessamad <elfadili.ae@gmail.com>
2023-03-21 11:44:52 -0700

    print to 98 added

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,64 @@
+#include"main.h"
+
+/**
+ * print_to_98 - print number to 98
+ *
+ * @n: starting int
+ *
+*/
+
+void print_to_98(int n)
+{
+	int m;
+
+	if (n <= 98 && n >= 0)
+		for (; n <= 98; n++)
+		{
+			if ((n / 10) != 0)
+				_putchar((n / 10) + 48);
+			_putchar((n % 10) + 48);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			if ((n / 100) != 0)
+				_putchar((n / 100) + 48);
+			_putchar((n / 10) % 10 + '0');
+			_putchar((n % 10) + 48);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (; n <= 98; n++)
+		{
+			m = n;
+			if (m < 0)
+			{
+				m = n * -1;
+				_putchar('-');
+			}
+			if ((m / 100) != 0)
+				_putchar((m / 100) + 48);
+			if ((m / 10) != 0)
+				_putchar((m / 10) % 10 + 48);
+			_putchar((m % 10) + 48);
+			if (m != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}

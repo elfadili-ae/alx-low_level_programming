f4e82bb20082ab02dfe2e905b7b0b834185c9021
Abdessamad <elfadili.ae@gmail.com>
2023-03-21 13:09:28 -0700

    n times table added

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,36 @@
+#include"main.h"
+
+/**
+ * times_table - print n times table
+ *
+ * @t: t times
+ *
+*/
+
+void print_times_table(int t)
+{
+	int m;
+	int n;
+	int res;
+
+	if (t > 0 && t < 15)
+		for (m = 0; m < (t + 1); m++)
+		{
+			for (n = 0; n < (t + 1); n++)
+			{
+				res = m * n;
+				if (n != 0)
+					if ((res / 10) == 0)
+						_putchar(' ');
+				if ((res / 10) != 0)
+					_putchar((res / 10) + 48);
+				_putchar((res % 10) + 48);
+				if (n != t)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+}

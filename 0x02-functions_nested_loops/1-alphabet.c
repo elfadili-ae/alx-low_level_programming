8452c1e6270acf671b8d52964c64e2747e4b45d5
Abdessamad <elfadili.ae@gmail.com>
2023-03-21 04:23:34 -0700

    print_alphabet function added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Description: print alphabet, in lowercase followed by new line.
+ *
+*/
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 #include <string.h>
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif

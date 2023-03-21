acf6c29509f52ec1a874ec364b4099f68fb7d398
Abdessamad <elfadili.ae@gmail.com>
2023-03-21 02:33:40 -0700

    print _putchar

---
+++ 0x01-variables_if_else_while/deletem.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print combinations of two two- digits
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (j != i)
+			{
+				putchar((i / 10) % 10 + '0');
+				putchar(i % 10 + 48);
+				putchar(' ');
+				putchar((j / 10) % 10 + '0');
+				putchar(j % 10 + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Main - Entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}

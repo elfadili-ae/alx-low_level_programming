466fc79e9b89428644a1bd08733467faaaf1578f
Abdessamad <elfadili.ae@gmail.com>
2023-03-21 07:59:30 -0700

    add function added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * add - print last digit
+ *
+ * Description: print last digit of a number
+ * @a: integer
+ * @b: integer
+ *
+ * Return: last digit (int)
+*/
+
+int add(int m, int n)
+{
+	return (m + n);
+}

a60b2c792fb9690f90a9c1d725778b5bc79631e6
Abdessamad <elfadili.ae@gmail.com>
2023-03-23 04:04:37 -0700

    Script: Add 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - print a line
+ * @c: length of the line
+ *
+ */
+void print_line(int c)
+{
+	int i;
+
+	for (i = 0; i < c; i++)
+		_putchar('-');
+	_putchar('\n');
+}

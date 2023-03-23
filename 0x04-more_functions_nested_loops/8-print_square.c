271fdc5b6beca15dbc8d9ebb1d3ca4b715e973c1
Abdessamad <elfadili.ae@gmail.com>
2023-03-23 04:56:02 -0700

    Script: Add 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - print a square of #
+ * @c: square size
+ */
+void print_square(int c)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < c; j++)
+	{
+		for (i = 0; i < c; i++)
+		{
+			_putchar('#');
+			if (i == (c - 1))
+				_putchar('\n');
+		}
+	}
+		_putchar('\n');
+}

9f532539a15d646d98129a69588a0c604a8d1894
Abdessamad <elfadili.ae@gmail.com>
2023-03-23 03:18:12 -0700

    Script: Add new script 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - calculate the multiplication of 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: @a * @b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

50359dda7ffb36fc264ef0be00aea8207ce64158
Abdessamad <elfadili.ae@gmail.com>
2023-03-23 12:01:23 -0700

    Fix: minor fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,36 +2,21 @@
 
 /**
  * print_number - print an int number
- *@num: number to print
+ *@d: number to print
  *
  */
-void print_number(int num)
+void print_number(int d)
 {
-	unsigned int length = 0;
-	int isNegative = 0;
-	int j;
-	int i;
+	unsigned int num = d;
 
-	if (num < 0)
+	if (d < 0)
 	{
-		isNegative = 1;
-		num = -num;
-	}
-	j = num;
-	while (j / 10)
-	{
-		j = j / 10;
-		length++;
-	}
-	if (isNegative)
 		_putchar('-');
-	for (j = length; j > 0; j--)
-	{
-		int expo = 1;
-
-		for (i = 0; i < j; ++i)
-			expo *= 10;
-		_putchar((num / expo) % 10 + '0');
+		num = num * -1;
 	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
 	_putchar(num % 10 + '0');
 }

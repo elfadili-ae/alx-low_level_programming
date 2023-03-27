041478ed8b84fdfe710d04ea30e80e45dd269e2d
elfadili-ae <elfadili.ae@gmail.com>
2023-03-27 14:23:46 +0000

    Script: 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - print every other character of a string
+ *@src: pointer to a string to copy
+ *@dest: pointer destination
+ *Return: return dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int l = strlen(src);
+
+	for (i = 0; i < l; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}

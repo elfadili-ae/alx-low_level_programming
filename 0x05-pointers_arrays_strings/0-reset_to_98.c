14eb5562a73717ab550c7915a18e1b2c8ca8d18c
elfadili-ae <elfadili.ae@gmail.com>
2023-03-27 06:38:58 +0000

    Style: update code style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98: reset value to 98 with a pointer
+ * reset_to_98 - reset value to 98 with a pointer
+ *@n: pointer to int
  */
-
 void reset_to_98(int *n)
 {
-  *n = 98;
+	*n = 98;
 }

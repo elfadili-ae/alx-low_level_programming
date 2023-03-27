d9186e2fcc405e0d79dbb4669bd94f9be2fddc28
elfadili-ae <elfadili.ae@gmail.com>
2023-03-27 14:11:11 +0000

    Fix: fixed the display

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,8 +8,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n > 0)
 	for (i = 0; i < n; i++)
 	{
-		printf("%d\n", a[i]);
+		printf("%d", a[i]);
+		if (i != (n-1))
+			printf(", ");
 	}
+	printf("\n");
 }

00ecdc460bf8d545c252e6702212e613fcb5966c
elfadili-ae <elfadili.ae@gmail.com>
2023-03-29 16:38:47 +0000

    Fix: script fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,11 +16,9 @@ int _strcmp(char *s1, char *s2)
 		len++;
 	while (i < len)
 	{
-		if (s1[i] > s2[i])
-			return (15);
-		else if (s1[i] < s2[i])
-			return (-15);
+		if (s1[i] != s2[i])
+			break;
 		i++;
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }

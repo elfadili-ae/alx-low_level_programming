a1d5304f6cc3b919d4f0d63c507304cc664f7211
elfadili-ae <elfadili.ae@gmail.com>
2023-03-29 20:21:53 +0000

    Fix: one if in code

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,19 +12,13 @@ char *rot13(char *str)
 
 	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		while ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
 		{
-			if ((str[i] + 13) > 122)
-				str[i] = 'a' + (str[i] + 13) - 123;
-			else
+			if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
 				str[i] += 13;
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			if ((str[i] + 13) > 90)
-				str[i] = 'A' + (str[i] + 13) - 91;
 			else
-				str[i] += 13;
+				str[i] -= 13;
+			i++;
 		}
 		i++;
 	}

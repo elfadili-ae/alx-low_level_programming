ec93355f324dae9b2f28f13d1c04069e1328dc28
elfadili-ae <elfadili.ae@gmail.com>
2023-03-29 23:40:28 +0000

    Script: changed method

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,18 +8,18 @@
 
 char *rot13(char *s)
 {
-		int i = 0;
+	int i = 0, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABDCEFGHIJKLMNOPQRSTUVWXYZ";
+	char r[] = "nopqrstuvwxyzabcdefghijklmNOQPRSTUVWXYZABCDEFGHIJKLM";
 
 	while (s[i] != '\0')
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-		{
-			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
-		}
+		for (j = 0; j < 52; j++)
+			if (s[i] == a[j])
+			{
+				s[i] = r[j];
+				break;
+			}
 		i++;
 	}
 	return (s);

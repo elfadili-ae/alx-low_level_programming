c8c1d70bc36b192c772589ecd9002809290a317f
Abdessamad <elfadili.ae@gmail.com>
2023-03-30 16:58:17 -0700

    Script: Added 104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_buffer - print the buffer of a string
+ * @b: string to print
+ * @size: buffer size
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i = 0, j = 0, k;
+
+	if (size <= 0)
+		putchar('\n');
+	for (i = 0; i < size; i += 10)
+	{
+		for (j = 0, k = 0; j < 10; j++)
+		{
+			if (j == 0)
+				printf("%08x: ", i);
+			if ((i + j) >= size)
+				printf("  ");
+			else
+				printf("%02x", b[i + j]);
+
+			if (k < 1)
+				k++;
+			else
+			{
+				k = 0;
+				putchar(' ');
+			}
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (b[i + j] >= 31 && b[i + j] <= 126)
+				putchar(b[i + j]);
+			else
+				putchar('.');
+		}
+		putchar('\n');
+	}
+}

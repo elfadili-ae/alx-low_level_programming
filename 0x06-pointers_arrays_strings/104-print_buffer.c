6b0621d6d7939f8cc4dcc5899381383842882ca2
Abdessamad <elfadili.ae@gmail.com>
2023-03-30 17:25:10 -0700

    Fix: break

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -33,6 +33,8 @@ void print_buffer(char *b, int size)
 		}
 		for (j = 0; j < 10; j++)
 		{
+			if ((i + j) >= size)
+				break;
 			if (b[i + j] >= 31 && b[i + j] <= 126)
 				putchar(b[i + j]);
 			else if ((i + j) < size)

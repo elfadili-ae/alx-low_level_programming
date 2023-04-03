c5a5969346cf757d5ca7c06e3183ba00c4cb597e
elfadili-ae <elfadili.ae@gmail.com>
2023-04-03 18:47:20 +0000

    Script: Added 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print the sum of diagonals
+ * @a: array
+ * @size: array size
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	unsigned long int sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[size * i  + i];
+	}
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[size * i + (size - 1 - i)];
+	}
+	printf("%ld, %ld\n", sum1, sum2);
+}

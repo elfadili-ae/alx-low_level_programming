31e91aa7c7ced9719633d4ffc87e0bc5c58e56fc
elfadili-ae <elfadili.ae@gmail.com>
2023-04-03 17:52:53 +0000

    Fix: print whole board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,10 +13,9 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (i == 0 || i == 7)
+			if (a[i][j] != ' ')
 				_putchar(a[i][j]);
-			if (i == 1 || i == 6)
-				_putchar(a[i][j]);
+
 		}
 		_putchar('\n');
 	}

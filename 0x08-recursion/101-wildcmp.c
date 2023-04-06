b433b81d593276d52f9e87cb1b0fa96559f72a10
elfadili-ae <elfadili.ae@gmail.com>
2023-04-06 00:54:01 +0000

    Script: Added 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* wildcmp - compare two strings
+* @s1: first string
+* @s2: second string (can contain wildcard)
+* Return: 1 (identical) 0 (not identical)
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 != *s2 && *s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s1 != *s2 && *s1 != '\0' && *s2 != '\0')
+	{
+		if (*(s2 - 1) == '*')
+			return (wildcmp(s1 + 1, s2));
+	}
+	return (0);
+}

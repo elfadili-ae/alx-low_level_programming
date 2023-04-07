b6a902943f2c973e464ce9cf9ebc9e056a233178
elfadili-ae <elfadili.ae@gmail.com>
2023-04-07 12:49:15 +0000

    Script: Added 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print multi of two arguments
+ * @argc: args count
+ * @argv: args array
+ * Return: 0 (sucess)
+ */
+int main(int argc, char *argv[])
+{
+	if ( argc == 3)
+		printf("%ld\n", atol(argv[1]) * atol(argv[2]));
+	else
+		printf("Error\n");
+	return (0);
+}

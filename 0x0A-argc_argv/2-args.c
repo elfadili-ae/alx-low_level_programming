a12acbf746e52b50464e997a8af4c9df7a2fadfe
elfadili-ae <elfadili.ae@gmail.com>
2023-04-07 12:32:12 +0000

    Script: Added 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print arguments
+ * @argc: args count
+ * @argv: args array
+ * Return: 0 (sucess)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}

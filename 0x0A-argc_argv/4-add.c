a671d8f39503f7121881642a70c63aedefbb0c11
elfadili-ae <elfadili.ae@gmail.com>
2023-04-07 16:04:46 +0000

    Fix: nevative numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,14 +13,16 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 	long num;
-	char *endptr = NULL;
+	char *endp = NULL;
+	char *np;
 
 	if (argc > 2)
 		for (i = 1; i < argc; i++)
 		{
+			np = argv[i];
 			errno = 0;
-			num = strtol(argv[i], &endptr, 10);
-			if (errno != 0 || endptr == argv[i] || argc[i] == '\0')
+			num = strtol(argv[i], &endp, 10);
+			if (errno != 0 || endp == argv[i] || *np == '\0')
 			{
 				printf("Error\n");
 				return (1);

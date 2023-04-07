4f70034c339f9545fea3c8eb454143e1becb67ab
elfadili-ae <elfadili.ae@gmail.com>
2023-04-07 19:14:43 +0000

    Script: Added 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - print the the min number of coins to make change for
+ *an amount of money
+ * @argc: args count
+ * @argv: args array
+ * Return: 0 (sucess) 1 (error)
+ */
+int main(int argc, char *argv[])
+{
+	int change = 0;
+	int money;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	money = atoi(argv[1]);
+	while (money > 0) {
+		if (money >= 25)
+		{
+			money -= 25;
+			change++;
+		}
+		else if (money >= 10)
+		{
+			money -= 10;
+			change++;
+		}
+		else if (money >= 5)
+		{
+			money -= 5;
+			change++;
+		}
+		else if (money >= 2)
+		{
+			money -= 2;
+			change++;
+		}
+		else
+		{
+			money -= 1;
+			change++;
+		}
+	}
+
+	printf("%d\n", change);
+	return 0;
+}

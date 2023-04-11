2cab5b46311160166b1eefd3e2e0c08f1b32df19
elfadili-ae <elfadili.ae@gmail.com>
2023-04-11 22:38:43 +0000

    Fix: Added element 0

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,14 +16,14 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 		len += strlen(av[i]) + 1;
 
 	txt = (char *) malloc(sizeof(char) * len);
 	if (txt == NULL)
 		return (NULL);
 
-	for (i = 1, len = 0; i < ac; i++)
+	for (i = 0, len = 0; i < ac; i++)
 	{
 		strcat(txt, av[i]);
 		strcat(txt, "\n");

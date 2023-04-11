9d351f4ba2a625221b5bc80cc83b7d6d4396bb89
elfadili-ae <elfadili.ae@gmail.com>
2023-04-11 12:00:07 +0000

    Fix: check malloc if failed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,19 +2,6 @@
 #include <string.h>
 
 /**
- * _malloc - allocate memory
- * @size: size
- * Return: pointer to allocated memory | NULL (fail)
- */
-char *my_malloc(int size)
-{
-	char *s;
-	s = malloc(size);
-	if (s == NULL)
-		return (NULL);
-	return (s);
-}
-/**
  * str_concat - concatenate tow strings
  * @s1: string
  * @s2: string
@@ -27,22 +14,30 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 	{
-		txt = my_malloc(1);
+		txt = malloc(1);
+		if (txt == NULL)
+			return (NULL);
 		strcpy(txt, "\0");
 	}
 	else if (s1 == NULL)
 	{
-		txt = my_malloc(strlen(s2) + 1);
+		txt = malloc(strlen(s2) + 1);
+		if (txt == NULL)
+			return (NULL);
 		strcpy(txt, s2);
 	}
 	else if (s2 == NULL)
 	{
-		txt = my_malloc(strlen(s1) + 1);
+		txt = malloc(strlen(s1) + 1);
+		if (txt == NULL)
+			return (NULL);
 		strcpy(txt, s1);
 	}
 	else
 	{
-		txt = my_malloc(strlen(s1) + strlen(s2) + 1);
+		txt = malloc(strlen(s1) + strlen(s2) + 1);
+		if (txt == NULL)
+			return (NULL);
 		strcpy(txt, s1);
 		strcat(txt, s2);
 	}

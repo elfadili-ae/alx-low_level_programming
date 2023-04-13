76318a4e397cd57c17f69373556127ca01d0abdd
elfadili-ae <elfadili.ae@gmail.com>
2023-04-13 17:12:28 +0000

    Fix: less than min

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -33,7 +33,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size != old_size && ptr != NULL)
 	{
-		for (i = 0; i <= min; i++)
+		for (i = 0; i < min; i++)
 			p[i] = cp[i];
 		free(ptr);
 	}

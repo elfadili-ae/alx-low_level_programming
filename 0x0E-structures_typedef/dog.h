cbde0c8ac913c9cb09dadd9798c2f9d510c96c6d
elfadili-ae <elfadili.ae@gmail.com>
2023-04-17 10:51:20 +0000

    Style: added struct dog description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ * struct dog - dog data
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;

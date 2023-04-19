3f07c55ee99172daa37554eaf7bf761c5a2c3c05
elfadili-ae <elfadili.ae@gmail.com>
2023-04-19 10:53:40 +0000

    Script: added 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - search for an integer
+ * @array: array
+ * @size: array size
+ * @cmp: compare function
+ * Return: index when match | -1 (failed)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}

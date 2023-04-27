a60e3c65bfcc7f55e810593874ba3d20d98730f0
elfadili-ae <elfadili.ae@gmail.com>
2023-04-27 11:07:15 +0000

    replaced strlen

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,21 +4,24 @@
 
 /**
  * add_node - number of elements in a linked list
- * @h: linked list
- * Return: number of elements in @h
+ * @head: linked list's head
+ * @str: string to copy in the new element
+ * Return: new head
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
+	int len = 0;
 
 	/*Allocate memory for newNode*/
-	newNode= (list_t *) malloc(sizeof(list_t));
+	newNode = (list_t *)malloc(sizeof(list_t));
 	if (newNode == NULL)
 		return (NULL);
 
 	/*Fill the newNode*/
 	newNode->str = strdup(str);
-	newNode->len = strlen(newNode->str);
+	while (newNode->str[len++] != '\0')
+		newNode->len = len;
 	newNode->next = *head;
 
 	*head = newNode;

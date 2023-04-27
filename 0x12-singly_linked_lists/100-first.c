5f2b8268d61fb7a7c40ffe8bca4798a50ca56cce
elfadili-ae <elfadili.ae@gmail.com>
2023-04-27 13:41:12 +0000

    fix betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,5 +6,7 @@
  */
 void __attribute__ ((constructor)) Awake()
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore ");
+	printf("my house upon my back!\n");
 }

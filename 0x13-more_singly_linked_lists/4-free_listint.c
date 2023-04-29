f1a49678017401edaabeb7a17380996ec233b5a1
elfadili-ae <elfadili.ae@gmail.com>
2023-04-29 11:00:46 +0000

    Script: added 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_nodeint - free a listint_t
+ * @head: listint_t's head
+ * Return: adress of the new node | NULL (failed)
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}

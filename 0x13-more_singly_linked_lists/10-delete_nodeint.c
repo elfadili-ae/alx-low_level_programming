8af9771e5b26f228ef9462f9b3f7c24d01ce4ffd
elfadili-ae <elfadili.ae@gmail.com>
2023-04-29 22:36:44 +0000

    Script: addded 10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * delete_nodeint_at_index - delete element at index
+ * @head: listint_t's head
+ * @n: int value
+ * Return: adress of the new node | NULL (failed)
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *prev, *delme, *curr = *head;
+	unsigned int i = 0;
+
+	if ((*head) == NULL)
+		return (-1);
+
+	if (index > 0)
+	{
+		while (curr->next != NULL)
+		{
+			if (i == (index -1))
+			{
+				break;
+			}
+			i++;
+			curr = curr->next;
+		}
+		prev = curr;
+		if (curr->next == NULL)
+			return (-1);
+		delme = curr->next;
+		if (delme->next == NULL)
+		{
+			free(delme);
+			prev->next = NULL;
+		}
+		else
+		{
+			prev->next = delme->next;
+			free(delme);
+		}
+	}
+	else
+	{
+		delme = (*head);
+		(*head) = (*head)->next;
+		free(delme);
+	}
+
+	return (1);
+}

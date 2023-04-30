f7ec42e7d291292c8297bd991f42f1970cd00c51
elfadili-ae <elfadili.ae@gmail.com>
2023-04-30 19:10:06 +0000

    Fix: reallocate and print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
+const listint_t **re_listint(const listint_t**, const listint_t*, int);
 /**
  * print_listint_safe - print the elements of listin_t safe mode
  *
@@ -11,24 +12,58 @@
  */
 size_t print_listint_safe(const listint_t *h)
 {
-	int nodes = 0;
+	int index, nodes = 0;
+	const listint_t **bak = NULL;
 
 	if (h == NULL)
-		exit(98);
+		return (0);
 
 	while (h != NULL)
 	{
-		printf("[%0#lx] %d\n", (uintptr_t)h, h->n);
-		if (h <= h->next)
+		for (index = 0; index < nodes - 1; index++)
 		{
-			h = h->next;
-			printf("-> [%0#lx] %d\n", (uintptr_t)h, h->n);
-			nodes++;
-			break;
+			if (h == bak[index])
+			{
+				free(bak);
+				printf("-> [%0#lx] %d\n", (uintptr_t)h, h->n);
+				return (nodes);
+			}
 		}
-
-		h = h->next;
 		nodes++;
+		printf("[%0#lx] %d\n", (uintptr_t)h, h->n);
+		bak = re_listint(bak, h, nodes);
+		h = h->next;
+
 	}
+	free(bak);
 	return (nodes);
+}
+
+/**
+ * re_listint - add element and re-allocate memory for listint_t
+ * @bak: listint_t's head
+ * @o: listint_t origin
+ * @size: list size
+ * Return: adress to the new list | NULL (failed)
+ */
+const listint_t **re_listint(const listint_t **bak, const listint_t *o, int len)
+{
+	const listint_t **newLinked;
+	int i = 0;
+
+	newLinked = malloc(sizeof(listint_t) * len);
+	if (newLinked == NULL)
+	{
+		free(bak);
+		exit(98);
+	}
+
+	while (i < (len - 1))
+	{
+		newLinked[i] = bak[i];
+		i++;
+	}
+	newLinked[i] = o;
+	free(bak);
+	return (newLinked);
 }

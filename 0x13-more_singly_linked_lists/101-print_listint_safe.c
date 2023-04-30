464a3849bc496c7c97b7eef8a247a8b28ac3f6a2
elfadili-ae <elfadili.ae@gmail.com>
2023-04-30 12:59:59 +0000

    Script: added 101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <inttypes.h>
+
+/**
+ * print_listint_safe - print the elements of listin_t safe mode
+ * @h: listint_t
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *h)
+{
+	int nodes = 0;
+
+	if (h == NULL)
+		return (98);
+
+	while (h != NULL)
+	{
+		printf("[%0#lx] %d\n", (uintptr_t)h, h->n);
+		if (h <= h->next)
+		{
+			h = h->next;
+			printf("-> [%0#lx] %d\n", (uintptr_t)h, h->n);
+			nodes++;
+			break;
+		}
+
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}

153ffa3b257f48184d5911d1dcd360e430b29e3c
elfadili-ae <elfadili.ae@gmail.com>
2023-05-04 11:05:29 +0000

    changed to set bitwise

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,35 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * binary_to_uint - convert number to binary
+ * binary_to_uint - convert binary to int
  * @b: number to convert
- * Return: binary convertion
+ * Return: int convertion
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int bi = 0;
-	int i;
+	int i = 0, len;
 
-	if (b[0] == '\0')
+	if (b == NULL || b[0] == '\0')
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	for (len = 0; b[len] != '\0'; len++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	while (i < len)
 	{
 		if (b[i] == '0' || b[i] == '1')
+		{
+			bi <<= 1;
+			bi |= b[i] - '0';
+			i++;
+		}
+		else
 			return (0);
-
-		bi <<= 1;
-		bi |= b[i] - '0';
 	}
 	return (bi);
 }

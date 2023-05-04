dc408d8e870bd60ffd69192406c7dff478b2455b
elfadili-ae <elfadili.ae@gmail.com>
2023-05-04 10:09:07 +0000

    a bit wiser

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert number to binary
+ * @b: number to convert
+ * Return: binary convertion
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned bi = 0;
+	int i;
+
+	if (b[0] == '\0')
+		return (0);
+
+	for(i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		bi <<= 1;
+		bi |= b[i] - '0';
+	}
+	return bi;
+}

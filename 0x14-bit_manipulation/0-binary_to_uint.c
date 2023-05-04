2ef7a82e1b82c74fd996b3a607ce62d81249a026
elfadili-ae <elfadili.ae@gmail.com>
2023-05-04 10:11:13 +0000

    Fix: var type and style

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,13 +7,13 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned bi = 0;
+	unsigned int bi = 0;
 	int i;
 
 	if (b[0] == '\0')
 		return (0);
 
-	for(i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
@@ -21,5 +21,5 @@ unsigned int binary_to_uint(const char *b)
 		bi <<= 1;
 		bi |= b[i] - '0';
 	}
-	return bi;
+	return (bi);
 }

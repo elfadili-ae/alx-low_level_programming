76edf4db8130f9e33756f1e2ea6abb9a52d2f295
elfadili-ae <elfadili.ae@gmail.com>
2023-05-08 19:14:41 +0000

    Script: program that copies a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,12 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+#define BUFFSIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+void errMessage(int errCode, char *str, int val);
+int _strlen(char *s, int j);
 #endif

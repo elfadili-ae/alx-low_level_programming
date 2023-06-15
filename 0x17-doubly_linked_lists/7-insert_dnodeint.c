406dbd2401600cddcb6bd6c7f32b38d5a93798dd
elfadili-ae <elfadili.ae@gmail.com>
2023-06-15 19:36:42 +0100

    Update: new approach using length of the list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,43 +10,49 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *newNode, *tmp = *h;
-	unsigned int i = 0, brCheck = 0;
+	unsigned int i = 0, len = dlistint_len(*h);
 
-	if (*h == NULL && idx != 0)
-		return NULL;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	while (tmp != NULL)
+	{
+		if (i == idx - 1)
+			break;
+		tmp = tmp->next;
+		i++;
+	}
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
-	newNode->n = n, newNode->next = NULL, newNode->prev = NULL;
 
-	if (idx > 0)
+	newNode->n = n;
+	if (idx > len)
 	{
-		while (tmp->next != NULL)
-		{
-			if (i == idx - 1)
-			{
-				brCheck = 1;
-				break;
-			}
-			tmp = tmp->next, i++;
-		}
-		if (!brCheck && i < idx - 1)
-		{
-			free(newNode);
-			return (NULL);
-		}
-		if (tmp != NULL)
-		{
-			newNode->next = tmp->next;
-			tmp->next = newNode;
-			newNode->prev = tmp;
-		}
-		if (newNode->next != NULL)
-			newNode->next->prev = newNode;
-	} else
-	{
-			newNode->next = tmp;
-			*h = newNode;
+		free(newNode);
+		return (NULL);
 	}
+	if (len == idx)
+		return (add_dnodeint_end(h, n));
+
+	tmp->next->prev = newNode;
+	newNode->next = tmp->next;
+	tmp->next = newNode;
 	return (newNode);
+}
+
+/**
+ * dlistint_len - count the length of doubly lniked list
+ * @h: head of the linked list
+ * Return: length of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }

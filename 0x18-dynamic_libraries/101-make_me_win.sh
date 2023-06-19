3ceb3bf8e0c50529c83288565fff542a743e105d
elfadili-ae <elfadili.ae@gmail.com>
2023-06-19 19:57:25 +0100

    Fix: make file executable

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
-wget https://github.com/elfadili-ae/alx-low_level_programming/raw/master/0x18-dynamic_libraries/print.so
-LD_PRELOAD=$PWD/print.so ./gm 9 8 10 24 75 9
+#!/bin/bash
+wget -P /tmp https://github.com/elfadili-ae/alx-low_level_programming/raw/master/0x18-dynamic_libraries/print.so
+export LD_PRELOAD=/tmp/print.so

dbcbd7aacc1a79b052af2dc6978ce5222821d1df
elfadili-ae <elfadili.ae@gmail.com>
2023-07-06 20:18:47 +0100

    Style: function description

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,19 +1,19 @@
 #include "hash_tables.h"
 
 /**
- * has_djb2 - an algorithm that generates a hash
- * @tr: string
+ * hash_djb2 - an algorithm that generates a hash
+ * @str: string
  * Return: hash
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }

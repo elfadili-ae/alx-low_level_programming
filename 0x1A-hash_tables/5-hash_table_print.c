3fdc14707e1b3445dd1d598c5a62a4421be51423
elfadili-ae <elfadili.ae@gmail.com>
2023-07-07 00:52:59 +0100

    Script: added 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print the hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int flag = 0;
+	hash_node_t *tmp;
+
+	if (ht == NULL || ht->size == 0)
+		printf("{}\n");
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flag)
+				printf(", ");
+
+			flag = 1;
+			/*printf("'%s': ", ht->array[i]->key);
+			  printf("'%s'",ht->array[i]->value);*/
+
+			tmp = ht->array[i];
+			while (tmp)
+			{
+				printf("'%s': ", tmp->key);
+				printf("'%s'",tmp->value);
+				tmp = tmp->next;
+			}
+		}
+	}
+	printf("}\n");
+}

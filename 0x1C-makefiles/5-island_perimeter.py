14086ee0d7debf72f24a9091951ccef9bbbe934a
elfadili-ae <elfadili.ae@gmail.com>
2023-08-23 13:29:06 +0100

    Fix: equal case

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,11 +12,11 @@ def island_perimeter(grid):
         for j in range(0, len(grid[i])):
             if grid[i][j] == 1:
                 permi += 4
-                if (i - 1) > 0 and grid[i - 1][j] == 1:
+                if (i - 1) >= 0 and grid[i - 1][j] == 1:
                     permi -= 1
                 if (i + 1) < len(grid) and grid[i + 1][j] == 1:
                     permi -= 1
-                if (j - 1) > 0 and grid[i][j - 1] == 1:
+                if (j - 1) >= 0 and grid[i][j - 1] == 1:
                     permi -= 1
                 if (j + 1) < len(grid[i]) and grid[i][j + 1] == 1:
                     permi -= 1
